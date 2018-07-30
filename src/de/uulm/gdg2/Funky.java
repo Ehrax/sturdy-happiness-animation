4669438f9dc171d132b6f244e82793250e579ecc
Alekei <alexander.rasputin@uni-ulm.de>
2018-07-30 20:41:54 +0200

    added json parser and working now on inner circle animation

--- src/de/uulm/gdg2/Funky.java
+++ src/de/uulm/gdg2/Funky.java
@@ -1,9 +1,9 @@
 package de.uulm.gdg2;
 
 import de.uulm.gdg2.controllers.Player;
-import de.uulm.gdg2.shapes.CustomLine;
 import de.uulm.gdg2.shapes.Poop;
 import de.uulm.gdg2.util.RGBaColor;
+
 
 import processing.core.PApplet;
 
@@ -11,29 +11,44 @@ public class Funky extends PApplet {
 
     private Player player;
 
+    public Poop poop;
+    public RGBaColor poopPrimaryColor;
+    public RGBaColor poopSecondaryColor;
+
+    public RGBaColor backgroundColor;
+
     @Override
     public void settings() {
         size(1240, 720);
+        smooth(8);
     }
 
     @Override
     public void setup() {
+        backgroundColor = new RGBaColor(255, 255, 255, 255);
 
-        this.background(255);
-        this.noStroke();
-        this.smooth();
+        background(backgroundColor.v1, backgroundColor.v2, backgroundColor.v3, backgroundColor.a);
 
+        noStroke();
         player = new Player(this, "./resources/song.mp3");
-
         player.startPlaying();
+
+        poopPrimaryColor = new RGBaColor(0, 0, 0, 255);
+        poopSecondaryColor = new RGBaColor(255, 255, 255, 255);
+        poop = new Poop(this,  poopPrimaryColor, poopSecondaryColor);
     }
 
     @Override
     public void draw(){
 
-        RGBaColor poopColor = new RGBaColor(0, 0, 0, 100);
-        Poop poop = new Poop(this, 50, poopColor);
+        // Color reset
+        background(backgroundColor.v1, backgroundColor.v2, backgroundColor.v3, backgroundColor.a);
 
+        /*
+         * TODO: this goes later to GUI
+         */
+        poop.draw();
+        poop.update(player.getSong().position());
     }
 
     public static void main(String[] args) {
---
+++ src/de/uulm/gdg2/controllers/AnimationImport.java
@@ -0,0 +1,68 @@
+package de.uulm.gdg2.controllers;
+
+import de.looksgood.ani.AniConstants;
+import de.looksgood.ani.easing.Easing;
+import de.uulm.gdg2.util.CustomAnimation;
+
+import java.util.ArrayList;
+
+import processing.core.PApplet;
+import processing.data.JSONArray;
+import processing.data.JSONObject;
+
+public abstract class AnimationImport {
+
+    public static ArrayList<CustomAnimation> importAnimation(PApplet canvas, String filePath, String type) {
+
+        JSONObject json = canvas.loadJSONObject(filePath);
+        JSONArray array = json.getJSONArray(type);
+
+        ArrayList<CustomAnimation> anisAnimations = new ArrayList<CustomAnimation>();
+
+        for (int i = 0; i < array.size(); i++) {
+            JSONObject o = array.getJSONObject(i);
+            float start = o.getFloat("start");
+            float duration = o.getFloat("end") - start;
+            float value = o.getFloat("value");
+            String easingString = o.getString("easing");
+            Easing easing = determineEasing(easingString);
+            CustomAnimation custAnimation = new CustomAnimation(start, duration, value, type, easing);
+
+            anisAnimations.add(custAnimation);
+        }
+
+        return anisAnimations;
+    }
+
+    private static Easing determineEasing(String ease) {
+        Easing e;
+
+        switch (ease) {
+            case "sine_in":
+                e = AniConstants.SINE_IN;
+                break;
+            case "sine_out":
+                e = AniConstants.SINE_OUT;
+                break;
+            case "cubic_out":
+                e = AniConstants.CUBIC_OUT;
+                break;
+            case "bounce_out":
+                e = AniConstants.BOUNCE_OUT;
+                break;
+            case "bounce_in_out":
+                e = AniConstants.BOUNCE_IN_OUT;
+                break;
+            case "elastic_out":
+                e = AniConstants.ELASTIC_OUT;
+                break;
+            case "elastic_in_out":
+                e = AniConstants.ELASTIC_IN_OUT;
+                break;
+            default:
+                e = AniConstants.LINEAR;
+                break;
+        }
+        return e;
+    }
+}
